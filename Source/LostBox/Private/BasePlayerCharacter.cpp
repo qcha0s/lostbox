29a77daa2a9b48f2f00d410789340234f47c5fe6
Unknown <patkellydesigns@gmail.com>
2017-09-25 20:01:44 -0400

    Initial

---
+++ Source/LostBox/Private/BasePlayerCharacter.cpp
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BasePlayerCharacter.h"
+
+ABasePlayerCharacter::ABasePlayerCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+
+// Called when the game starts or when spawned
+void ABasePlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABasePlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABasePlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+// Calculate Health Function
+void ABasePlayerCharacter::CalculateHealth(float Delta)
+{
+	Health += Delta;
+	CalculateDead();
+}
+
+// Calculate Dead Function
+void ABasePlayerCharacter::CalculateDead()
+{
+	if (Health <= 0)
+		isDead = true;
+	else
+		isDead = false;
+}
+
+// PostEditChangeProperty
+
+#if WITH_EDITOR
+void ABasePlayerCharacter::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	isDead = false;
+	Health = 100;
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	// did the changes kill this character?
+	CalculateDead();
+}
+
+#endif
+
